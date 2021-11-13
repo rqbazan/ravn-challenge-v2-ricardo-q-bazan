0f49d576d5115190a90209f724d4afc0a4b4b5d2
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2021-11-13 16:35:47 -0500

    feat(DataCell): support undefined `value` prop

--- src/ui/components/data-cell/index.tsx
+++ src/ui/components/data-cell/index.tsx
@@ -5,14 +5,14 @@ import { Cell, Typography } from '~/ui/core'
 export interface DataCellProps {
   className?: string
   label: string
-  value: string
+  value?: string
 }
 
 export function DataCell({ label, value, className, ...props }: DataCellProps) {
   return (
     <Cell {...props} className={clsx(className, 'flex justify-between')}>
       <Typography variant="h2LowEmphasis">{label}</Typography>
-      <Typography variant="h2">{value}</Typography>
+      {value && <Typography variant="h2">{value}</Typography>}
     </Cell>
   )
 }
